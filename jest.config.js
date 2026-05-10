d4dd40ec99f7d26494bc3fc7a2c4b49342a541c3
AshenSandeep <ashen60438@gmail.com>
2026-05-10 13:20:47 +0530

    fix: errs in the test suite for the form validation

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  transformIgnorePatterns: [
+    'node_modules/(?!(' +
+      '@react-native|' +
+      'react-native|' +
+      '@react-navigation|' +
+      'react-native-safe-area-context|' +
+      '@react-native-async-storage|' +
+      '@react-native-community' +
+    ')/)',
+  ],
+  moduleNameMapper: {
+    '@react-native-async-storage/async-storage':
+      '@react-native-async-storage/async-storage/jest/async-storage-mock',
+    '@react-native-community/netinfo':
+      '__mocks__/@react-native-community/netinfo.js',
+  },
 };
